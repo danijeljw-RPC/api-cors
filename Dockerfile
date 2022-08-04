c1135ba09dc908f95ba04283db664a02400681ad
danijeljw-rpc <danijel@repasscloud.com>
2022-08-04 15:57:51 +0100

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ RUN dotnet publish -c Release -o out
 
 # Build runtime image
 FROM mcr.microsoft.com/dotnet/aspnet:6.0
+EXPOSE 8080
 WORKDIR /app
 COPY --from=build-env /app/out .
-EXPOSE 80
 ENTRYPOINT ["dotnet", "api-cors.dll"]
--- Program.cs
+++ Program.cs
@@ -16,11 +16,14 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        // if (app.Environment.IsDevelopment())
+        // {
+        //     app.UseSwagger();
+        //     app.UseSwaggerUI();
+        // }
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseAuthorization();
 
